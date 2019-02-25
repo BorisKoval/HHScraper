68e49c13a78d3a7b222a52f26f1ec58d725e126d
Boris <boris_bfh@mail.ru>
2019-02-25 22:09:02 +0300

    del comments

--- hhParserStart.py
+++ hhParserStart.py
@@ -1,8 +1,4 @@
-#scrapy runspider hhParser.py -o quotes.json --- в параметры запуска приложения
-
-#запуск паука парсера
-#from scrapy.cmdline import execute
-#execute(['scrapy','runspider', 'hhParserCore.py']) #работает старт !!     , '-o qqq.json'
+#scrapy runspider hhParser.py -o quotes.json ---
 
 import urllib.request
 import json
@@ -14,12 +10,11 @@ def GetStats():
     urlIT = r'https://api.hh.ru/vacancies?area=88&clusters=true&enable_snippets=true&specialization=1&from=cluster_professionalArea'
     responseIT_vacs = urllib.request.urlopen(urlIT)
 
-    #print(responseIT_vacs.read())  чтение всего возврата апи
+    #print(responseIT_vacs.read())  
 
     data = json.load(responseIT_vacs)
-    print(data['found']) #кол-во
+    print(data['found'])
 
-    #конект и вставка в БД
     mydb = mysql.connector.connect(host="192.168.0.20", user="root"
                                    , passwd="19031994", database="hhVacs")
     mycursor = mydb.cursor()
@@ -35,9 +30,8 @@ def GetVacancies():
     responseIT_vacs = urllib.request.urlopen(urlIT)
 
     data = json.load(responseIT_vacs)
-    #print(data['found']) #кол-во
-    #print(data['items']) #все элементы
-    #конект и вставка в БД
+    #print(data['found']) 
+    #print(data['items']) 
 
     print(data['items']['id'])
 
